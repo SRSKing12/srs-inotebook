5c9fadd4a1d8a1da822fbcef7bf3184cc4987feb
Somesh Sharma <85858871+SRSKing12@users.noreply.github.com>
2022-03-01 20:15:43 +0530

    Update db.js

--- db.js
+++ db.js
@@ -5,9 +5,7 @@ const MONGO_URI = process.env.MONGO_URI
 const connectToMongo = () => {
     mongoose.connect(MONGO_URI, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false
+        useUnifiedTopology: true
     }).then(() => {
         console.log("Connected to Mongo Successfully !")
     }).catch((err) => {console.log(err)})

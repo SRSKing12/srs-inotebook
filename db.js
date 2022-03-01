965447fe40099b19ce25537914c42c3e957933e4
Somesh Sharma <85858871+SRSKing12@users.noreply.github.com>
2022-03-01 20:05:50 +0530

    Update db.js

--- db.js
+++ db.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const MONGO_URI = process.env.MONGO_URI
 
 const connectToMongo = () => {
-    mongoose.connect(MONGO_URI, ()=>{
-        console.log("Connected to Mongo successfully!");
-    })
+    mongoose.connect(MONGO_URI, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false
+    }).then(() => {
+        console.log("Connected to Mongo Successfully !")
+    }).catch((err) => {console.log("Unable to connect to Mongo!")})
 }
 
 module.exports = connectToMongo;
